013c451bc402ee57c0c89d6e72572d91c6d54231
abhinavgahoi <abhinav.gahoi09@gmail.com>
2024-08-28 14:40:32 +0530

    UNET MODEL ADDED

---
+++ UNET_Architecture.py
@@ -0,0 +1,51 @@
+def build_unet(inputs, ker_init, dropout):
+    # Encoder - extracts basic features and reduces the spatial dimensions
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(inputs)
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv1)
+    pool = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    #2ND CONVOLUTION - extract more complex features
+    conv = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool)
+    conv = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv)
+
+    #3RD CONVOLUTION - extract more hidden features
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    #4TH CONVOLUTION - extracts very high level features
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv3)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    #BOTTLENECK LAYER - Captures the most hidden features
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool4)
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv5)
+    drop5 = Dropout(dropout)(conv5)
+
+    #DECODER - reconstructs the spatial dimensions
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = 2)(drop5))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = 2)(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = 2)(conv8))
+    merge9 = concatenate([conv,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv9)
+
+    up = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = 2)(conv9))
+    merge = concatenate([conv1,up], axis = 3)
+    conv = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge)
+    conv = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv)
+
+    # FINAL LAYER - Produces the segmentation map with class probabilities.
+    conv10 = Conv2D(4, 1, activation = 'softmax')(conv)
+
+    return Model(inputs = inputs, outputs = conv10)
